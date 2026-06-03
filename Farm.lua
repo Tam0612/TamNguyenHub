019df267c8ed9b771cec4b8614de5880335f3c8d
Tam0612 <htam75906@gmail.com>
2026-06-03 18:18:14 +0700

    Add initial Farm.lua script with metadata

---
+++ Farm.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "TamNguyenHub"
+getgenv().ScriptSubTitle = "Discord https://discord.gg/BUyKMcdxd"
+getgenv().ScriptImage = "https://i.ibb.co/n8LBRVvM/1000160831.png"
+getgenv().ScriptAuthorName = "Nguyễn Hoàng Tâm"
+getgenv().ScriptAuthorSubTitle = "Game Vui Vẻ"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
